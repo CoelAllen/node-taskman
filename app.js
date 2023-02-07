cff05bdf551814f60ae18abf1f9edd24ce38ae73
Coel Allen <coelallen@gmail.com>
2023-02-07 12:46:40 -0700

    planning api routes

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const app = express();
 
+// Routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager");
+});
+
+// Going to need put, post, get, getById and delete
+// app.get('')-------------get all tasks
+// app.post('')------------make new task
+// app.get(':id')----------getTaskById
+// app.put/patch(':id')---edit task
+// app.delete(':id')-------delete task
+
 const port = 3000;
+
+// @ts-ignore
+app.listen(port, console.log(`Server is listening on port ${port}`));
