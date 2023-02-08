623a665cde3278b768d6b26a117e080f3ab604fd
Coel Allen <coelallen@gmail.com>
2023-02-08 14:10:26 -0700

    CRUD working

--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -1,24 +1,47 @@
 const Task = require("../models/Task.js");
 
 const newTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getAllTasks = (req, res) => {
-  res.send("all items from TasksController");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTaskById = (req, res) => {
-  res.json({ id: req.params.id });
+const getTaskById = async (req, res) => {
+  try {
+    res.status(200).json(await Task.findById(req.params.id));
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const editTask = (req, res) => {
-  res.send("edit task");
+const editTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json("Successfully updated");
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const removeTask = (req, res) => {
-  res.send("delete task");
+const removeTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete(req.params.id);
+    res.status(200).json("Task successfully deleted");
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
